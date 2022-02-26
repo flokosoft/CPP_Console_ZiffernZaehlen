fd54f71a8ecd0bc329f66919600ce3bcce090c62
Florian Koch <github@flokosoft.com>
2022-02-26 13:31:01 +0100

    Logikfehler behoben

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
-#include <ctime>
 #include <chrono>
 
 int main() {
-    int x = 1;
     std::string CharCount;
-    unsigned int WriteChar = 0;
     bool NewInput = true;
     bool InputCorrect = false;
     std::string NewRound;
 
     while(NewInput) {
-        int UserInput;
+        unsigned int UserInput = 0;
+        unsigned int x = 1;
+        unsigned int WriteChar = 0;
+
         std::cout << "Bis wieviel soll gezählt werden?\nDeine Eingabe: ";
         std::cin >> UserInput;
         if(std::cin.fail()) {
@@ -34,14 +34,13 @@ int main() {
         if(x >= UserInput && InputCorrect) {
             auto iTimestampStop = std::chrono::system_clock::now();
             std::chrono::duration<double> elapsed_seconds = iTimestampStop - iTimestampStart;
-            std::time_t end_time = std::chrono::system_clock::to_time_t(iTimestampStop);
 
-            std::cout << "Es wurde bis " << UserInput << " gezählt.\nDabei wurden " << WriteChar
-                      << " Ziffern geschrieben!" << std::endl;
+            std::cout << "Es wurde bis " << UserInput << " gezählt.\nDabei wurden " << WriteChar << " Ziffern geschrieben!" << std::endl;
             std::cout << "Der Vorgang hat insgesamt: " << elapsed_seconds.count() << " Sekunden benötigt!" << std::endl;
-            std::cout << "Neue Eingabe? y/n:";
+            std::cout << "Neue Eingabe? j/n:";
             std::cin >> NewRound;
-            if (NewRound == "y") {
+
+            if (NewRound == "y" || NewRound == "j") {
                 NewInput = true;
             } else {
                 NewInput = false;
