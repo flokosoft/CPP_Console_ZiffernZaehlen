81b9eaeb3c6030ff0254ae9aca68cfa84a832a55
Florian Koch <github@flokosoft.com>
2022-03-02 16:50:51 +0100

    Using Namespace

--- main.cpp
+++ main.cpp
@@ -1,44 +1,45 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+using namespace std;
 
 int main() {
-    std::string CharCount;
+    string CharCount;
     bool NewInput = true;
     bool InputCorrect = false;
-    std::string NewRound;
+    string NewRound;
 
     while(NewInput) {
         unsigned int UserInput = 0;
         unsigned int x = 1;
         unsigned int WriteChar = 0;
 
-        std::cout << "Bis wieviel soll gezählt werden?\nDeine Eingabe: ";
-        std::cin >> UserInput;
-        if(std::cin.fail()) {
+        cout << "Bis wieviel soll gezählt werden?\nDeine Eingabe: ";
+        cin >> UserInput;
+        if(cin.fail()) {
             InputCorrect = false;
         }
         else {
             InputCorrect = true;
         }
 
-        auto iTimestampStart = std::chrono::system_clock::now();
+        auto iTimestampStart = chrono::system_clock::now();
 
         while (x <= UserInput && InputCorrect)  {
-            std::cout << "Ich bin bei " << x << std::endl;
-            CharCount = std::to_string(x);
+            cout << "Ich bin bei " << x << endl;
+            CharCount = to_string(x);
             WriteChar += CharCount.size();
             x++;
         }
 
         if(x >= UserInput && InputCorrect) {
-            auto iTimestampStop = std::chrono::system_clock::now();
-            std::chrono::duration<double> elapsed_seconds = iTimestampStop - iTimestampStart;
+            auto iTimestampStop = chrono::system_clock::now();
+            chrono::duration<double> elapsed_seconds = iTimestampStop - iTimestampStart;
 
-            std::cout << "Es wurde bis " << UserInput << " gezählt.\nDabei wurden " << WriteChar << " Ziffern geschrieben!" << std::endl;
-            std::cout << "Der Vorgang hat insgesamt: " << elapsed_seconds.count() << " Sekunden benötigt!" << std::endl;
-            std::cout << "Neue Eingabe? j/n:";
-            std::cin >> NewRound;
+            cout << "Es wurde bis " << UserInput << " gezählt.\nDabei wurden " << WriteChar << " Ziffern geschrieben!" << endl;
+            cout << "Der Vorgang hat insgesamt: " << elapsed_seconds.count() << " Sekunden benötigt!" << endl;
+            cout << "Neue Eingabe? j/n:";
+            cin >> NewRound;
 
             if (NewRound == "y" || NewRound == "j") {
                 NewInput = true;
@@ -47,7 +48,7 @@ int main() {
             }
         }
         else {
-            std::cout << "Ungültige Eingabe. Beende das Programm" << std::endl;
+            cout << "Ungültige Eingabe. Beende das Programm" << endl;
             return(EXIT_SUCCESS);
         }
     }
